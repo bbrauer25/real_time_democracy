a79d3d6f372752e3ce5578ea7aa6c015418132fe
Kara Franco <kara.a.franco@gmail.com>
2016-03-11 10:51:43 -0500

    Sign up page connected to main page

--- real_time_main.js
+++ real_time_main.js
@@ -11,7 +11,7 @@ app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 //app.set('views', __dirname + '/views');
 //app.set('view engine', 'jade');
-app.set('port', 3000);
+app.set('port', 3001);
 app.use(express.static('static')); //static pages for testing
 app.use(sessions({
   cookieName: 'session',
@@ -25,28 +25,103 @@ app.use(sessions({
 app.get('/', function (req, res, next) {
   //screen for whether user is logged in
   if (req.session && req.session.username) { //user based home landing page
-    var issueQuery = "SELECT title, issue FROM RTD_issue;";
+    var issueQuery = "SELECT title, id, issue FROM RTD_issue;";
     mysql.pool.query(issueQuery, function (err, rows) {
       if (err) {
         next(err);
         return;
       }
+      var issueArray = [];
       if (rows.length > 0) {
         console.log(rows[0]);
         var data = {
           username: req.session.username,
           issue_description: rows[0].issue,
-          issue_title: rows[0].title
+          issue_title: rows[0].title,
+          issue_id: rows[0].id
         };
+        for (var i = 0; i < rows.length; i++) {
+          //issueArray.push("<article class=\"issue\"><h3>" + rows[i].title + "</h3><p>" + rows[i].issue + "</p></article>");
+          var myArticle = "<article class=\"issue\"><h3>" + rows[i].title + "</h3><p>" + rows[i].issue + "</p>";
+          myArticle += "<form class=\"view-issue\" action=\"/viewDetail\" method=\"post\"><input type=\"submit\" value=\"View Issue Detail\" class=\"view-issue-button\">";
+          myArticle += "<input class=\"issue\" name=\"issue_id\" type=\"hidden\" value=" + rows[i].id + "></form></article>";
+          issueArray.push(myArticle);
+        };
+        data.issue = issueArray;
         res.render('home_page', data);
-      } else {
-          var data = {username: req.session.username};
-          res.render('home_page', data)
       }
     });
   } else {  //serve home.html if not
     res.sendFile('static/home.html', { root : __dirname});
   }
+});
+
+app.post('/viewDetail', function(req, res) {
+  //expects issue_id to be posted as a body parameter
+  var viewDetailQuery = "SELECT id, title, issue FROM RTD_issue WHERE id =" + req.body.issue_id + ";";
+  console.log(viewDetailQuery);
+  
+  //screen to see if comment_text is set and add to database if sort
+  if (req.body.comment_text) {
+    var commentInsertQuery = "INSERT INTO RTD_issue_comment(user_id, issue_id, comment) VALUES(";
+    commentInsertQuery += req.session.user_id + "," + req.body.issue_id + ",\"" + req.body.comment_text + "\");";
+    console.log(commentInsertQuery);
+    mysql.pool.query(commentInsertQuery, function (err, results) {
+      if (err) return;
+      console.log("successfully added comment");
+    });
+  }
+  
+  mysql.pool.query(viewDetailQuery, function (err, rows) {
+    if (err) return;
+    if (rows < 1) {
+      res.send("no issue found in database");
+    } else {
+      var commentsArray = [];
+      var data = {
+        issue_title: rows[0].title,
+        issue_description: rows[0].issue,
+        issue_upvotes: 0,
+        issue_id: req.body.issue_id,
+        comments: []
+      };
+      currentUpvotesQuery = "SELECT * FROM RTD_issue_upvote WHERE issue_id = " + req.body.issue_id + ";";
+      mysql.pool.query(currentUpvotesQuery, function (err, rows) {
+        data.issue_upvotes = rows.length;
+        //get comments 
+        commentsQuery = "SELECT comment FROM RTD_issue_comment WHERE issue_id = " + req.body.issue_id + ";";
+        console.log(commentsQuery);
+        mysql.pool.query(commentsQuery, function (err, rows) {
+          if (err) return;
+          if (rows.length > 0) {
+            for (var i = 0; i < rows.length; i++) {
+              var myComment = "<p>" + rows[i].comment + "</p>";
+              commentsArray.push(myComment);
+            }
+            data.comments = commentsArray;
+            res.render('viewDetail', data);
+          } else {
+            res.render('viewDetail', data);
+          }
+        });
+      });
+    }
+  });
+});
+
+app.post('/sendUpvote', function(req, res) {
+  var userID = req.session.user_id;
+  //first send the vote
+  var sendUpvoteQuery = "INSERT INTO RTD_issue_upvote(user_id, issue_id, upvote) VALUES(";
+  sendUpvoteQuery += userID + "," + req.body.issue_id + ",\"1\");";
+  mysql.pool.query(sendUpvoteQuery, function (err, result) {
+    if (err) return;
+    currentUpvotesQuery = "SELECT * FROM RTD_issue_upvote WHERE issue_id = " + req.body.issue_id + ";";
+    mysql.pool.query(currentUpvotesQuery, function (err, rows) {
+      if (err) return;
+      res.send("Upvotes: " + rows.length);
+    });
+  });
 });
 
 app.post('/logout', function(req, res) {
@@ -68,6 +143,7 @@ app.post('/login', function (req, res, next) {
       res.send('Sorry, credentials are invalid');
     } else {
       //now check for password
+      var userID = rows[0].id;
       var passwordQuery = "SELECT id FROM RTD_password WHERE user_id = " + rows[0].id;
       passwordQuery += " AND password = \"" + req.body.password + "\";";
       mysql.pool.query(passwordQuery, function (err, rows) {
@@ -81,29 +157,88 @@ app.post('/login', function (req, res, next) {
             req.session.reset();
           }
           req.session.username = req.body.username;
-          var issueQuery = "SELECT title, issue FROM RTD_issue;";
+          req.session.user_id = userID;
+          var issueQuery = "SELECT title, id, issue FROM RTD_issue;";
           mysql.pool.query(issueQuery, function (err, rows) {
             if (err) {
               next(err);
               return;
             }
+              
             if (rows.length > 0) {
               console.log(rows[0]);
-              var data = {
-                username: req.session.username,
-                issue_description: rows[0].issue,
-                issue_title: rows[0].title
-              };
-              res.render('home_page', data);
+              var issueArray = [];
+              if (rows.length > 0) {
+                console.log(rows[0]);
+                var data = {
+                  username: req.session.username,
+                  issue_description: rows[0].issue,
+                  issue_title: rows[0].title,
+                  issue_id: rows[0].id
+                };
+                for (var i = 0; i < rows.length; i++) {
+                  var myArticle = "<article class=\"issue\"><h3>" + rows[i].title + "</h3><p>" + rows[i].issue + "</p>";
+                  myArticle += "<form class=\"view-issue\" action=\"/viewDetail\" method=\"post\"><input type=\"submit\" value=\"View Issue Detail\" class=\"view-issue-button\">";
+                  myArticle += "<input class=\"issue\" name=\"issue_id\" type=\"hidden\" value=" + rows[i].id + "></form></article>";
+                  //issueArray.push("<article class=\"issue\"><h3>" + rows[i].title + "</h3><p>" + rows[i].issue + "</p></article>");
+                  issueArray.push(myArticle);
+                };
+                data.issue = issueArray;
+                res.render('home_page', data);
+              }
             } else {
                 var data = {username: req.session.username};
-                res.render('home_page', data)
+                res.render('signup_complete', data)
             }
           });
         }
       });
     }
   });
+});
+
+// basic signup page when the user hits the submit button, POST is sent
+app.get('/sign-up', function (req, res, next) {
+    res.render('sign_up');
+    //res.sendFile('static/signup.html', { root : __dirname});   
+});
+
+// POST request is sent to DB, the user info is entered
+app.post('/sign-up', function (req, res, next) {
+    // this is printing to the console with correct information
+    console.log(req.body.fname);
+    console.log(req.body.user);
+
+    // conditional, to check what user_type has been selected from dropdown menu
+    if (req.body.user === "constituent") {
+        var signupQuery = "INSERT INTO RTD_user (f_name, l_name, email, zip, role_type_id) VALUES ('" + req.body.fname + "','" + req.body.lname + "', '" + req.body.email + "', '" + req.body.zip + "', (SELECT id FROM RTD_role_type WHERE role_name = '" + "constituent" + "'))";
+        console.log(signupQuery);
+        mysql.pool.query(signupQuery, function (err, rows) {
+            if (err) {
+                next(err);
+                return;
+            }
+            if (rows < 1) {
+                req.session.reset();
+                res.send('Sorry, sign up failed');
+            }
+        });
+    }
+
+    // conditional, to check what user_type has been selected from dropdown menu
+    if (req.body.user === "official") {
+        var signupQuery = "INSERT INTO RTD_user (f_name, l_name, email, zip, role_type_id) VALUES ('" + req.body.fname + "','" + req.body.lname + "', '" + req.body.email + "', '" + req.body.zip + "', (SELECT id FROM RTD_role_type WHERE role_name = '" + "elected" + "'))";
+        mysql.pool.query(signupQuery, function (err, rows) {
+            if (err) return;
+
+            if (rows < 1) {
+                req.session.reset();
+                res.send('Sorry, sign up failed');
+            }
+        });
+    }
+
+    res.render('home_page');
 });
 
 app.use(function (req, res) {
@@ -121,4 +256,5 @@ app.use(function (err, req, res, next) {
 app.listen(app.get('port'), function () {
     console.log('Express started on http://localhost:' + app.get('port') + '; press Ctrl-C to terminate.');
 });
+
 
